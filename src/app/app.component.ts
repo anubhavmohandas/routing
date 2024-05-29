1ba6c6108e2824da57041f92b63ee4da69ad5855
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-05-29 07:10:43 +0530

    final_exam routing_sem4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, HomeComponent, ListComponent, ShoppingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'home', component:HomeComponent},
+    {path:'list', component:ListComponent},
+    {path:'shopping', component:ShoppingComponent}
+];
